d4977b3a620c68416671c4211d0593fec4ec1fb6
Petr Sevostianov <ps@antilatency.com>
2020-03-11 23:00:38 +0300

    *

--- Update.bat
+++ Update.bat
@@ -1,2 +1,3 @@
+git pull
 git submodule update --init --recursive
 git submodule foreach -q --recursive "git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master) && git pull"
