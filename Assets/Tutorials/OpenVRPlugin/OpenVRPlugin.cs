ef35f468484689ed2aa7bd23374c80719504f37a
Nikita Baskakov <ooogpo@gmail.com>
2020-04-17 15:16:21 +0300

    [Content] OpenVR Plugin description

---
+++ Assets/Tutorials/OpenVRPlugin/OpenVRPlugin.cs
@@ -0,0 +1,22 @@
+﻿using Csml;
+
+
+partial class Tutorials : Scope {
+    public static MultiLanguageGroup OpenVRPlugin => new MultiLanguageGroup("OpenVR Plugin");
+    public partial class OpenVRPlugin_Assets : Scope {
+        public static Code DirectoryStructure => new Code(@"antilatency/
+├── bin /
+│   └── ...
+├── resources /
+│   └── ...
+└── driver.vrdrivermanifest", ProgrammingLanguage.Markdown);
+
+        public static Code SteamVrConfigExample => Code.FromFile("steamvr.json", ProgrammingLanguage.JavaScript);
+
+        public static Image CreateSystemReportScreen => new Image("steamvr_create_system_report.png");
+        public static Image SystemReportScreen => new Image("steamvr_system_report.png");
+    }
+
+
+
+}
