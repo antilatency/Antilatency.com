454e48ac30d4c63d2404a74d11ee10ace3f59172
Denis Kuznetsov <denis.kuznetsov@antilatency.com>
2020-05-29 18:29:37 +0300

    Added AntilatencyIpTrackingDemoProviderRaspberryPi tutorial.

---
+++ Assets/Tutorials/AntilatencyIpTrackingDemoProviderRaspberryPi/Antilatency_Ip_Tracking_Demo_Provider_RaspberryPi.cs
@@ -0,0 +1,42 @@
+using Csml;
+
+partial class Tutorials : Scope {
+    public static LanguageSelector<IMaterial> Antilatency_Ip_Tracking_Demo_Provider_RaspberryPi => new LanguageSelector<IMaterial>();
+
+    public partial class Antilatency_Ip_Tracking_Demo_Provider_RaspberryPi_Assets : Scope {       
+        public static Code TrackingDemoOutput => new Code(
+            @"/opt/antilatency/bin/AntilatencyIpTrackingDemoProvider -v
+[15:27:31.905][000741][info] Start factory creation
+Polling Thread Id 1982088240
+[15:27:31.909][000741][info] Device / sys / bus / usb / devices / 1 - 1 / init completed.
+...
+[15:27:33.219][000741][info] Launched task a6c07d30-e3cf-44ed-b996-5f96998392e2 on 2 Node
+id: rand706031805; c_time: 6649237; error: ; gpio: 001000011111000111100100
+        tag: Head; err: None; posX: 0; posY: 0; posZ: 0; rotX: 0; rotY: 0; rotZ: 0; rotW: 1
+id: rand706031805; c_time: 6649438; error: ; gpio: 001000011111000111100100
+        tag: Head; err: None; posX: 0.000280265; posY: -0.000177345; posZ: -2.28101e-05; rotX: -2.59319e-05; rotY: -2.59936e-06; rotZ: 1.15174e-05; rotW: 1
+id: rand706031805; c_time: 6649639; error: ; gpio: 001000011111000111100100
+        tag: Head; err: None; posX: 0; posY: 1.68; posZ: 0; rotX: 0.0440574; rotY: -5.36158e-06; rotZ: 0.52934; rotW: 0.847265"
+            , ProgrammingLanguage.PowerShell
+        );
+
+        public static Code ProviderConf => new Code(
+            @"# Multicast group
+--receivers=239.255.111.111
+
+# UnityUdpTrackingReceiver port
+--port=56789
+
+# DevKit
+--environment=AAVSaWdpZBcABnllbGxvdwQEBAABAQMBAQEDAAEAAD_W
+
+# ~60 FPS
+--wait-time=16
+
+# Set GPIO pins default state
+--gpio=25:output:high"
+            , ProgrammingLanguage.PowerShell
+        );
+
+    }
+}
---
+++ Assets/Tutorials/AntilatencyIpTrackingDemoProviderRaspberryPi/Antilatency_Ip_Tracking_Demo_Provider_RaspberryPi_en.cs
@@ -0,0 +1,56 @@
+﻿using Csml;
+using static Tutorials.Antilatency_Ip_Tracking_Demo_Provider_RaspberryPi_Assets;
+
+partial class Tutorials : Scope {
+
+    public static Material Antilatency_Ip_Tracking_Demo_Provider_RaspberryPi_en => new Material("A demo application that provides data from Antilatency Tracker to network hosts", null, $"")
+
+        [new Section("Antilatency.IpTrackingDemo.Provider.RaspberryPi")
+            [$"{new ExternalReference("https://github.com/antilatency/Antilatency.IpTrackingDemo.Provider.RaspberryPi", "Antilatency.IpTrackingDemo.Provider.RaspberryPi")} is a demo application that provides data from {Hardware.Alt} connected to Raspberry Pi single board computer to network hosts."]
+        ]
+
+        [new Section("Preparation")
+            [new UnorderedList()
+                [$"Get Raspberry Pi 3 or 4 single board computer (RPi)."]
+                [$"Prepare microSD card with modified OS image ({Tutorials.PrepareRaspberryPiSdCard})."]
+                [$"Get fast WiFi router. {new ExternalReference("https://en.wikipedia.org/wiki/IEEE_802.11ac", "802.11ac 5 GHz mode")} support is highly recommended."]
+                [$"Power on RPi then {new ExternalReference("https://www.raspberrypistarterkits.com/how-to/find-raspberry-pi-ip-address/", "find out")} board IP address."]
+                [$"{new ExternalReference("https://github.com/antilatency/Antilatency.IpTrackingDemo.Provider.RaspberryPi/blob/master/BUILD.md", "Build and deploy")} AntilatencyIpTrackingDemoProvider app or just download binaries from GitHub release pages (place {new ExternalReference("https://github.com/antilatency/Antilatency.IpTrackingDemo.Provider.RaspberryPi/releases", "app")} to `/opt/antilatency/bin`, {new ExternalReference("https://github.com/antilatency/Antilatency.RaspberryPiSdk.Cpp/releases", "libs")} to `/opt/antilatency/lib`)."]
+            ]
+        ]
+
+        [new Section("Send position and rotation to network hosts")
+            [$"It is possible to place tens of Antilatency trackers (Alt) with corresponding amount of RPi’s in the same tracking area. Each RPi can communicate with Alt through USB connection, compute actual position from raw sensors data then send the position to remote hosts via built-in WiFi module. There is the AntilatencyIpNetwork (AIP) library which simplified network communication setup."]
+        ]
+
+        [new Section("Getting it all together")
+            [$"{new ExternalReference("https://github.com/antilatency/Antilatency.IpTrackingDemo.Provider.RaspberryPi", "Antilatency.IpTrackingDemo.Provider.RaspberryPi")} demo shows how to obtain tracking data and GPIO state from RPi board, send it via network connection, detect USB reconnection, handle errors. Then you need to setup receiver using the same multicast group address to obtain tracking data from remote AntilatencyIpTrackingDemoProvider app."]
+            [$"Start demo app (verbose output):"]
+            [TrackingDemoOutput]
+            [$"You can set:"]
+            [new UnorderedList()
+                [$"receiver IP (multicast group) and port (default: `--receivers=239.255.111.X --port=56789`),"]
+                [$"sending rate (default: `--wait-time=16` milliseconds, approximate 60 FPS),"]
+                [$"tracking area (default: `--environment-code=AAVSaWdpZBcABnllbGxvdwQEBAABAQMBAQEDAAEAAD_W` {new ExternalReference ("https://antilatency.com/getting-started/latest/area#content", "DevKit")},"]
+                [$"device ID (default: `--identifier=MAC_ADDR`),"]
+                [$"GPIO pins state (for example: `--gpio=25:output:high` - sets {new ExternalReference("https://pinout.xyz/pinout/wiringpi", "WiringPi pin 25")} to output digital UP output mode)"]
+                [$"path to a configuration file (for example: `--config=/etc/opt/antilatency/provider.conf`)."]
+            ]
+            [$"`/etc/opt/antilatency/provider.conf` example:"]
+            [ProviderConf]
+        ]
+
+        [new Section("See also")
+            [new UnorderedList()
+                [$"{new ExternalReference("https://github.com/antilatency/Antilatency.IpTrackingDemo.Provider.RaspberryPi/blob/master/BUILD.md", "Build instruction")}."]
+                [$"{Tutorials.PrepareRaspberryPiSdCard} - raspbian OS image modification."]
+                [$"AntilatencyIpNetwork library."]
+                [$"{new ExternalReference("https://www.youtube.com/watch?v=1VfFXoJiGFU", "Practical usage demonstration")}."]
+            ]
+        ]
+
+        ;
+
+    
+
+}
