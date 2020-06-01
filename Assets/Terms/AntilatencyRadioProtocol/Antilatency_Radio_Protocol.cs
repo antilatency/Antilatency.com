32e3f436324a722e18406f76b95a509464f87691
Andrey Desyatov <da@antilatency.com>
2020-06-01 13:36:55 +0300

    Change Radio Protocol images from PNG to JPG

--- Assets/Terms/AntilatencyRadioProtocol/Antilatency_Radio_Protocol.cs
+++ Assets/Terms/AntilatencyRadioProtocol/Antilatency_Radio_Protocol.cs
@@ -2,7 +2,7 @@ using Csml;
 
 partial class Terms {
     public static LanguageSelector<IMaterial> Antilatency_Radio_Protocol => new LanguageSelector<IMaterial>();
-    public static Image AntilatencyRadioProtocolTopology => new Image("./RadioDevicesScheme01.png");
+    public static Image AntilatencyRadioProtocolTopology => new Image("./RadioDevicesScheme01.jpg");
     public static Image RadioChannelsImage => new Image("./RadioChannels.jpg");
-    public static Image MultipleChannelsImage => new Image("./MultipleChannels.png");
+    public static Image MultipleChannelsImage => new Image("./MultipleChannels.jpg");
 }
