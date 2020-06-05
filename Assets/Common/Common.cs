30a85a331149e59264bca7fcdeef2b14dcf6633f
Nikita Baskakov <ooogpo@gmail.com>
2020-06-05 11:44:19 +0300

    footer test

--- Assets/Common/Common.cs
+++ Assets/Common/Common.cs
@@ -7,6 +7,7 @@ namespace Csml {
     partial class Scope {
         public static ITemplate Template => new Csml.TemplateRegularMaterial(Root.MainMenu)
             [Root.LanguageMenu]
+            [Root.CommonFooter]
             ;
     }
 
@@ -18,17 +19,18 @@ partial class Root : Scope {
 
     public new static ITemplate Template => new Csml.TemplateLanding(MainMenu)
         [Root.LanguageMenu]
+        [Root.CommonFooter]
             ;
 
     //public Root() {
-        //Material.DefaultTemplate = RegularPage;
+    //Material.DefaultTemplate = RegularPage;
     //}
 
 
     public static Menu MainMenu => new Menu()
         [LogoGreenBlack]
         [Index]
-        
+
         [Terms.Antilatency_Device_Network]
         [Hardware.Alt]
         [Internal.Debug]
@@ -42,7 +44,36 @@ partial class Root : Scope {
     public static LanguageMenu LanguageMenu => new LanguageMenu();
 
 
+    public static Footer CommonFooter => new Footer()
+        [new FooterMenu()
+            [LogoGreenBlack]
+        ]
+        
+        [new FooterMenu()
+            [new FooterMenuSection("SECTION 1")
+                [new UnorderedList()
+                    [Terms.Antilatency_Device_Network]
+                    [Hardware.Alt]
+                    [Internal.Debug]
+                    [CsmlPredefined.Diagnostics]
+                    [Software.Antilatency_Service]
+                ]
+            ]
 
+            [new FooterMenuSection("SECTION 2")
+                [new UnorderedList()
+                    [Store.PresetEditor]
+                    [Sdk.Configurator]
+                    [Api.Material]
+                ]
+            ]
+
+            [new FooterMenuSection("")
+                [$"Copyright © 2020, ALT LLC. info@antilatency.com"]
+            ]
+        ]
+
+        ;
 
 }
 
