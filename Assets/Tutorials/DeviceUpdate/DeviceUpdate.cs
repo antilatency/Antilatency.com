18a937a1958ae7943a84d8c8dcbeafc458bb379a
Roman Sokolov <rs@antilatency.com>
2020-06-17 16:35:19 +0300

    add DeviceUpdate tutorial

---
+++ Assets/Tutorials/DeviceUpdate/DeviceUpdate.cs
@@ -0,0 +1,15 @@
+using Csml;
+
+
+partial class Tutorials : Scope {
+    public static LanguageSelector<IMaterial> Device_Update => new LanguageSelector<IMaterial>();
+    public partial class Device_Update_Assets : Scope {
+        
+        public static Image DeviceTree => new Image("./DeviceTree.jpg");
+        public static Image ReflashFirmware => new Image("./ReflashFirmware.jpg");
+        public static Image SelectFirmware => new Image("./SelectFirmware.jpg");
+        public static YoutubeVideo TitleVideo => new YoutubeVideo("zq3w-tXyk4w");
+        
+    }
+    }
+    
