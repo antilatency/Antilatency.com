e5546fc0cefd410f3b79c8ab58d7b876ffc4c8ac
Roman Sokolov <rs@antilatency.com>
2020-06-18 16:18:35 +0300

    add Device_Tree

---
+++ Assets/Terms/DeviceTree/Device_Tree.cs
@@ -0,0 +1,9 @@
+using Csml;
+
+partial class Terms {
+    public static LanguageSelector<IMaterial> Device_Tree => new LanguageSelector<IMaterial>();
+
+    public static Image RadioScheme => new Image("./RadioScheme.png");
+    public static Image DeviceHierarchy => new Image("./DeviceHierarchy.jpg");
+    
+}
