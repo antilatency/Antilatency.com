e98303b9ac89247522265da48f8f4fd10ad79e2d
Roman Sokolov <rs@antilatency.com>
2020-06-22 16:44:22 +0300

    add new tutorial: AltHMDPlacement

---
+++ Assets/Tutorials/AltHMDPlacement/AltHMDPlacement.cs
@@ -0,0 +1,27 @@
+using Csml;
+
+
+partial class Tutorials : Scope {
+    public static LanguageSelector<IMaterial> Alt_HMD_Placement => new LanguageSelector<IMaterial>();
+    public partial class Alt_HMD_Placement_Assets : Scope {
+        public static Image TitleImage => new Image("./TitleImage.jpg");
+        
+        public static Image ASPlacement => new Image("./ASPlacement.png");
+        public static Image ASSelect => new Image("./ASSelect.png");
+        public static Image Axises => new Image("./Axises.png");
+        public static Image CorrectPlacementScene => new Image("./CorrectPlacementScene.png");
+        public static Image IncorrectPlacementScene => new Image("./IncorrectPlacementScene.png");
+        public static Image OculusGoPlacement => new Image("./OculusGoPlacement.jpg");
+        public static Image OculusQuestPlacement => new Image("./OculusQuestPlacement.jpg");
+        public static Image OculusRiftPlacement => new Image("./OculusRiftPlacement.jpg");
+        public static Image OculusRiftSPlacement => new Image("./OculusRiftSPlacement.jpg");
+        public static Image PicoG2Placement => new Image("./PicoG2Placement.jpg");
+        public static Image ViveProPlacement => new Image("./ViveProPlacement.jpg");
+
+
+        
+    
+        
+    }
+    }
+    
