1dae948caf6de544d8bad474623e0339333e50d3
Roman Sokolov <rs@antilatency.com>
2020-07-13 19:02:58 +0300

    Добавил туториал по установке кастомной площадки на ValveIndex. Не конечный вариант еще, нужно доработать.

---
+++ Assets/Tutorials/ValveIndexCustomSocketPlatform/ValveIndexCustomSocketPlatform.cs
@@ -0,0 +1,20 @@
+﻿using Csml;
+
+
+partial class Tutorials : Scope {
+    public static LanguageSelector<IMaterial> Valve_Index_Custom_Socket_Platform => new LanguageSelector<IMaterial>();
+    public partial class Valve_Index_Custom_Socket_Platform_Assets: Scope {
+        public static Image TitleImage => new Image("./TitleImage.jpg");
+        public static Image ValveIndexBottomPlatformStep1 => new Image("./ValveIndexBottomPlatformStep1.jpg");  
+        public static Image ValveIndexBottomSocketPlatformAssembly => new Image("./ValveIndexBottomSocketPlatformAssembly.jpg");
+        public static Image ValveIndexBottomSocketPlatformInstall => new Image("./ValveIndexBottomSocketPlatformInstall.jpg");    
+        public static Image ValveIndextopSocketPlatformAssembly => new Image("./ValveIndextopSocketPlatformAssembly.jpg");  
+        public static Image ValveIndextopSocketPlatformInstall => new Image("./ValveIndextopSocketPlatformInstall.jpg");  
+        public static Image ValveIndexControllerSocketPlatform => new Image("./ValveIndexControllerSocketPlatform.jpg"); 
+               
+        
+    }
+
+
+
+}
