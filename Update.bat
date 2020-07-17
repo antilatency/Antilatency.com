b40caf4cceef65ed6b31be7d37221fe3cc52e69d
maziev1 <maziev@mail.ru>
2020-07-17 14:16:32 +0300

    bla bla

--- Update.bat
+++
@@ -1,3 +0,0 @@
-git pull
-git submodule update --init --recursive
-git submodule foreach -q --recursive "git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master) && git pull"
