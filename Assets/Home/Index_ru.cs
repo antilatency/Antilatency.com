34f252c6dc007bcde8aa0a38fa8c3278f5c27c72
l-proger <l-proger@yandex.ru>
2020-08-12 22:41:41 +0300

    Revert Ci test changes

--- Assets/Home/Index_ru.cs
+++ Assets/Home/Index_ru.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using static Root.Index_Assets;
 
 public partial class Root {
-    public static Material Index_ru => new Material("Antilatency", null, $"Компания мечты CI test")
+    public static Material Index_ru => new Material("Antilatency", null, $"Компания мечты")
         [TitleVideo.GetPlayer().ConfigureAsBackgroundVideo()]
 
         [LogoWhiteBlack]
