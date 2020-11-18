432047808c745d262044842f76bc8f92cf482477
tatianayaroshenko <tatiana.yaroshenko@antilatency.com>
2020-11-18 16:17:11 +0300

    Adn and Ahei minor changes.

--- Assets/Terms/AntilatencyHardwareExtensionInterface/Antilatency_Hardware_Extension_Interface.cs
+++ Assets/Terms/AntilatencyHardwareExtensionInterface/Antilatency_Hardware_Extension_Interface.cs
@@ -4,5 +4,6 @@ partial class Terms : Scope {
     public static LanguageSelector<IMaterial> Antilatency_Hardware_Extension_Interface => new LanguageSelector<IMaterial>();
 
     public partial class Antilatency_Hardware_Extension_Interface_Assets : Scope {
+        
     }
 }
