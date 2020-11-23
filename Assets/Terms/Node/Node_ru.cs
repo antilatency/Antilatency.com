d2b033fa1177b3d451d0c624f24af8b623d21669
tatianayaroshenko <tatiana.yaroshenko@antilatency.com>
2020-11-23 16:00:10 +0300

    Add a definition for the Node.

--- Assets/Terms/Node/Node_ru.cs
+++ Assets/Terms/Node/Node_ru.cs
@@ -3,10 +3,12 @@ using static Api.Antilatency.DeviceNetwork;
 
 public partial class Terms {
 
-public static Material Node_ru => new Material("Node", null, $"")
+public static Material Node_ru => new Material("Node", null, $"Узел в {Terms.Device_Tree}, которым может выступать любое из устройств Antilatency, подключённых к одному {Terms.Host}.")
 
-[$"При подключении к сети каждое устройство получает идентификатор {NodeHandle.NameRefCode}. Идентификаторы не повторяются в процессе работы одного экземпляра сети."]
-
-;
+[$"Все устройства Antilatency, подключённые к одному Host, образуют сеть. При подключении к этой сети, каждое устройство получает идентификатор {NodeHandle.NameRefCode}. Идентификаторы не повторяются в процессе работы одного экземпляра сети."]
+[$"В частных случаях некоторые устройства могут отображаться в дереве устройств как два отдельных узла. Например, {Hardware.HMD_Radio_Socket} в режиме клиента, подключённый к Host одновременно через точку доступа и по USB, отобразится дважды под разными именами."]
+[new Info($"Подробнее читайте тут: {Tutorials.HMDSocketMode}")]
+[$"Любой из узлов дерева устройств может содержать {Terms.Task}."]
+; 
 
 }
