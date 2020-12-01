e15ba1f8e111c148920b7c653ebf02d10593f268
tatianayaroshenko <tatiana.yaroshenko@antilatency.com>
2020-12-01 09:55:53 +0300

    Add a description of the SocketUsb(ru).

--- Assets/Hardware/SocketUsb/SocketUsb.cs
+++ Assets/Hardware/SocketUsb/SocketUsb.cs
@@ -4,4 +4,11 @@ partial class Hardware : Scope {
 
     public static LanguageSelector<IMaterial> SocketUsb => new LanguageSelector<IMaterial>();
 
+    public partial class SocketUsb_Assets : Scope{
+
+        public static string Dimensions = "9 x 18 x 32";
+        public static string Weight = "";
+
+    }
+
 }
--- Assets/Hardware/SocketUsb/SocketUsb_ru.cs
+++ Assets/Hardware/SocketUsb/SocketUsb_ru.cs
@@ -1,7 +1,24 @@
 using Csml;
+using static Hardware.SocketUsb_Assets;
 
 public partial class Hardware{
 
-public static Material SocketUsb_ru => new Material("Socket USB", null, $"");
+public static Material SocketUsb_ru => new Material("Socket USB", null, $"Лёгкий и маленький Socket без поддержки {Terms.Antilatency_Radio_Protocol} идёт в комплекте с {Hardware.Alt}.")
 
+[new Section("Техническая спецификация")
+                [new Table(2)
+                    [$"Интерфейс связи"][@$"USB 2.0 Full Speed"]
+                    [$"Разъём"][$"USB Type-C port (питание и передача данных)"]
+                    [$"Аккумулятор"][@$"Нет"]
+                    [$"Поддержка Antilatency Hardware Extension Interface"][$"Нет"]
+                    [$"Питание"][$"USB 5В"]
+                    [$"Энергопотребление"][@$"Без {Hardware.Alt}: 15мA@5В
+                                                С подключённым {Hardware.Alt}: 115мA@5В"]
+                    [$"Индикация"][@$"RGB LED"]
+                    [$"Габариты"][$"{Dimensions} мм"]
+                    [$"Вес"][$"{Weight} г"]
+                ]
+            ]
+
+;
 }
