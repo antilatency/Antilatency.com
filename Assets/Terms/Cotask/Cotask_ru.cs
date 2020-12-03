d721f92b04c553a252bf108b968fe16590972329
tatianayaroshenko <tatiana.yaroshenko@antilatency.com>
2020-12-03 12:35:51 +0300

    Minor changes.

--- Assets/Terms/Cotask/Cotask_ru.cs
+++ Assets/Terms/Cotask/Cotask_ru.cs
@@ -4,7 +4,7 @@ public partial class Terms{
 
 public static Material Cotask_ru => new Material("Cotask", null, $"Элемент библиотеки {Terms.Antilatency_Device_Network}, который предоставляет пользователю интерфейс для работы с {Terms.Task}, запущенном на {Terms.Node}.")
 
-[$"Если Task может быть запущен на любом узле {Terms.Device_Tree}, то Cotask запускается на {Host}-устройстве. "]
+[$"Если Task может быть запущен на любом узле {Terms.Device_Tree}, то Cotask запускается на {Host}."]
 ;
 
 }
