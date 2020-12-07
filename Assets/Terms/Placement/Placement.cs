ac3788c6789c4b88a0304c438034895acfa53bda
tatianayaroshenko <tatiana.yaroshenko@antilatency.com>
2020-12-07 12:12:58 +0300

    Added a definition of the Placement.

--- Assets/Terms/Placement/Placement.cs
+++ Assets/Terms/Placement/Placement.cs
@@ -3,5 +3,9 @@ using Csml;
 public partial class Terms : Scope{
 
     public static LanguageSelector<IMaterial> Placement => new LanguageSelector<IMaterial>();
+
+    public partial class Placement_Assets : Scope {
+
+    }
     
 }
