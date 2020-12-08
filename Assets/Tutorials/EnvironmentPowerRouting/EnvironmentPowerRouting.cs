bedbbc997e1f3dc43e008ba34493a0a39d18e4c5
tatianayaroshenko <tatiana.yaroshenko@antilatency.com>
2020-12-08 17:26:48 +0300

    Minor changes.

--- Assets/Tutorials/EnvironmentPowerRouting/EnvironmentPowerRouting.cs
+++ Assets/Tutorials/EnvironmentPowerRouting/EnvironmentPowerRouting.cs
@@ -6,7 +6,6 @@ partial class Tutorials : Scope {
     public partial class EnvironmentPowerRouting_Assets : Scope {
         public static Image TitleImage => new Image("./Images/TitleImage.jpg");
         
-        public static Image ConnectionScheme => new Image("./Images/ConnectionScheme.jpg");
         public static Image OpenEnvironmentEditor => new Image ("./Images/OpenEnvironmentEditor.gif");
         public static Image RoutingTab => new Image ("./Images/RoutingTab.png");
         public static Image LoadRouting => new Image ("./Images/LoadRouting.png");
@@ -14,6 +13,8 @@ partial class Tutorials : Scope {
         public static Image SocketPlacementModeOff => new Image ("./Images/SocketPlacementModeOn.gif");
         public static Image Evaluate => new Image ("./Images/Evaluate.gif");
         public static Image SaveRouting => new Image ("./Images/SaveRouting.png");
+        public static Image DeleteRoutingElement => new Image("./Images/DeleteRoutingElements.gif");
+        public static Image UndoAndRedoOptions => new Image("./Images/UndoAndRedoOptions.gif");
 
     }
     }
