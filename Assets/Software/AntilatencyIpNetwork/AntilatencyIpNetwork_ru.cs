0567b06379cd9a1ae97bca498dac2e852a7a1a54
tatianayaroshenko <tatiana.yaroshenko@antilatency.com>
2020-12-08 12:15:14 +0300

    Added ru version for the AntilatencyIpNetwork's description.

--- Assets/Software/AntilatencyIpNetwork/AntilatencyIpNetwork_ru.cs
+++ Assets/Software/AntilatencyIpNetwork/AntilatencyIpNetwork_ru.cs
@@ -7,19 +7,26 @@ partial class Software : Scope {
 
         [new Section("API")
             [$"Интерфейс библиотеки для объекта NetworkServer, который может отправлять и получать данные о состоянии устройств, подключённых к той же сети. Например, информацию о положении устройства в пространстве."]
-            [$""]
-            [$"ILibrary interface produces INetworkServer interface (getNetworkServer method). INetworkServer can “listen” the network to get messages from other devices in LAN (startMessageListening then getDeviceList methods). Also it can send message with data from all connected position trackers (sendStateMessages method)."]
-            [$"StateMessage structure represents a tracking data: data source name (32 bytes UTF8 string), Cartesian coordinates (in meters, float), rotation quaternion components (float) and error code. To get fixed size representation of a standard string for the data source name (StateMessage.rawTag) you need to use getRawTagFromString method from ILibrary interface."]
+
+            [$"Интерфейс INetworkServer наследует интерфейс ILibrary. INetworkServer может прослушивать сеть и получать сообщения от других устройств в локальной сети с помощью методов startMessageListening и getDeviceList. Кроме этого, он может отправлять данные о позиции со всех подключенных трекеров, используя метод sendStateMessages."]
+
+            [$"StateMessage содержит в себе имя источника данных (32-байтовая строка UTF8), декартовы координаты (в метрах, с плавающей точкой), компоненты квартерниона вращения (с плавающей точкой) и код ошибки."]
+           
+            [$"Чтобы узнать фиксированный размер стандартной строки с именем источника данных (StateMessage.rawTag), используйте метод getRawTagFromString интерфейса ILibrary."]
+            
             [ApiDesc]
         ]
 
-        [new Section("Realization details")
-            [$"A {new ExternalReference("https://en.wikipedia.org/wiki/User_Datagram_Protocol", "UDP")} socket is used for network exchange. All data is transmitted to an IP address of a {new ExternalReference("https://en.wikipedia.org/wiki/Multicast", "multicast group")}. The device should join the multicast group to receive data."]
-            [$"We suggested to use the default multicast group address:"]
-            [$"`Antilatency::IpNetwork::Constants::DefaultTargetAddress` - the one from unassigned part 239.255.0.0/16 of local {new ExternalReference("https://tools.ietf.org/html/rfc2365", "Administratively Scoped Multicast Space")}."]
+        [new Section("Детали реализации")
+
+        [$"Socket с {new ExternalReference("https://en.wikipedia.org/wiki/User_Datagram_Protocol", "UDP")} используется для обмена данными по сети. Пакеты данных передаются на IP-адрес мультикастной группы. Поэтому, чтобы получать данные, устройство должно быть частью многоадрессной группы."]
+
+        [$"Мы рекомендуем использовать адрес мультикастной группы, установленный по умолчанию:"]
+    
+            [$"`Antilatency::IpNetwork::Constants::DefaultTargetAddress` - незарезервированная часть 239.255.0.0/16 локальной {new ExternalReference("https://tools.ietf.org/html/rfc2365", "административно-ограниченной многоадрессной группы")}."]
         ]
 
-        [new Section("Example")
+        [new Section("Пример")
             [$"Заголовочные файлы:"]
             [ExampleHeaders]
             [$"Загрузка библиотек:"]
