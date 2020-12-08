624759cf0ef7e24aff83ac968ef2df279cbab01a
tatianayaroshenko <tatiana.yaroshenko@antilatency.com>
2020-12-08 16:20:03 +0300

    Edited AltHMDPlacement.

--- Assets/Tutorials/AltHMDPlacement/AltHMDPlacement.cs
+++ Assets/Tutorials/AltHMDPlacement/AltHMDPlacement.cs
@@ -4,25 +4,26 @@ using Csml;
 partial class Tutorials : Scope {
     public static LanguageSelector<IMaterial> Alt_HMD_Placement => new LanguageSelector<IMaterial>();
     public partial class Alt_HMD_Placement_Assets : Scope {
-        public static Image TitleImage => new Image("./TitleImage.jpg");
-        
-        public static Image ASPlacement => new Image("./ASPlacement.png");
-        public static Image ASSelect => new Image("./ASSelect.png");
-        public static Image Axises => new Image("./Axises.png");
-        public static Image CorrectPlacementScene => new Image("./CorrectPlacementScene.png");
-        public static Image IncorrectPlacementScene => new Image("./IncorrectPlacementScene.png");
-        public static Image OculusGoPlacement => new Image("./OculusGoPlacement.jpg");
-        public static Image OculusQuestPlacement => new Image("./OculusQuestPlacement.jpg");
-        public static Image OculusRiftPlacement => new Image("./OculusRiftPlacement.jpg");
-        public static Image OculusRiftSPlacement => new Image("./OculusRiftSPlacement.jpg");
-        public static Image PicoG2Placement => new Image("./PicoG2Placement.jpg");
-        public static Image ViveProPlacement => new Image("./ViveProPlacement.jpg");
-         public static Image PicoG2Socket => new Image("./PicoG2Socket.jpg");
+        public static Image TitleImage => new Image("./Images/TitleImage.jpg");
 
-
+        public static Image CreateCustomPlacement => new Image ("./Images/CreateCustomPlacement.gif");
+        public static Image CreateCustomPlacement2 => new Image("./Images/CreateCustomPlacement2.png");
+        public static Image SetPlacement => new Image("./Images/SetPlacement.png");
         
-    
         
-    }
+        public static Image ASPlacement => new Image("./Images/ASPlacement.png");
+        public static Image ASSelect => new Image("./Images/ASSelect.png");
+        public static Image Axises => new Image("./Images/Axises.png");
+        public static Image CorrectPlacementScene => new Image("./Images/CorrectPlacementScene.png");
+        public static Image IncorrectPlacementScene => new Image("./Images/IncorrectPlacementScene.png");
+        public static Image OculusGoPlacement => new Image("./Images/OculusGoPlacement.jpg");
+        public static Image OculusQuestPlacement => new Image("./Images/OculusQuestPlacement.jpg");
+        public static Image OculusRiftPlacement => new Image("./Images/OculusRiftPlacement.jpg");
+        public static Image OculusRiftS => new Image("./Images/OculusRiftS.jpg");
+        public static Image PicoG2Placement => new Image("./Images/PicoG2Placement.jpg");
+        public static Image ViveProPlacement => new Image("./Images/ViveProPlacement.jpg");
+        public static Image PicoG2Socket => new Image("./Images/PicoG2Socket.jpg");    
+        
+        }
     }
     
