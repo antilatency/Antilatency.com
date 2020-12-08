df7c1022426613375e7fcf3ea346082af49bfd6d
tatianayaroshenko <tatiana.yaroshenko@antilatency.com>
2020-12-08 14:57:39 +0300

    Edited TrackingAreaSetup

--- Assets/Tutorials/TrackingAreaSetup/TrackingAreaSetup.cs
+++ Assets/Tutorials/TrackingAreaSetup/TrackingAreaSetup.cs
@@ -4,12 +4,17 @@ using Csml;
 partial class Tutorials : Scope {
     public static LanguageSelector<IMaterial> Tracking_Area_Setup => new LanguageSelector<IMaterial>();
     public partial class Tracking_Area_Setup_Assets : Scope {
-        public static Image TitleImage => new Image("./TitleImage.jpg");
-        public static Image AntilatencyServiceScreen => new Image("./AntilatencyServiceScreen.jpg");
-        public static Image EnvironmentEditorSelectRouting => new Image("./EnvironmentEditor - Routing select.jpg");
-        public static Image ConnectionScheme => new Image("./ConnectionScheme.jpg");
-        public static Image EnvironmentEditorScreen => new Image("./EnvironmentEditorScreen.jpg");
-        public static Image RoutingScreen => new Image("./RoutingScreen.png");
+        public static Image TitleImage => new Image("./Images/TitleImage.jpg");
+        
+        public static Image ConnectionScheme => new Image("./Images/ConnectionScheme.jpg");
+        public static Image OpenEnvironmentEditor => new Image ("./Images/OpenEnvironmentEditor.gif");
+        public static Image RoutingTab => new Image ("./Images/RoutingTab.png");
+        public static Image LoadRouting => new Image ("./Images/LoadRouting.png");
+        public static Image SocketPlacementModeOn => new Image ("./Images/SocketPlacementModeOn.gif");
+        public static Image SocketPlacementModeOff => new Image ("./Images/SocketPlacementModeOn.gif");
+        public static Image Evaluate => new Image ("./Images/Evaluate.gif");
+        public static Image SaveRouting => new Image ("./Images/SaveRouting.png");
+
     }
     }
     
